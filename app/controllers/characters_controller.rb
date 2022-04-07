fc3ae0df997ed557aa84a5fe0ac2d496742b8852
josesison1 <learnacademy@LEARNs-MacBook-Air.local>
2022-04-07 16:45:28 -0700

    completed endpoints

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,34 @@
 class CharactersController < ApplicationController
+    def index
+        characters = Character.all
+        render json: characters
+    end
+
+    def create
+        character = Character.create(strong_character_params)
+        if character.valid?
+        render json: character
+        else 
+        render json: character.errors
+        end
+    end
+
+    def update
+        character = Character.find(params[:id])
+        character.update(strong_character_params)
+        if character.valid?
+            render json: character
+        else 
+            render json: character.errors
+        end
+    end
+
+    def destroy
+
+    end
+
+    private
+    def strong_character_params 
+        params.require(:character).permit(:name, :age, :enjoys, :image)
+    end
 end
--- spec/requests/characters_spec.rb
+++ spec/requests/characters_spec.rb
@@ -2,6 +2,61 @@ require 'rails_helper'
 
 RSpec.describe "Characters", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it"gets a list characters" do
+      Character.create(
+        name: 'Jose',
+        age: '5',
+        enjoys: 'running',
+        image: 'http://d279m997dpfwgl.cloudfront.net/wp/2017/11/jose-running-1000x800.jpg'
+      )
+      get '/characters'
+
+      character = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(character.length).to eq 1
+    end
   end
+
+  describe "POST /create" do
+    it"creates a custom character" do
+      character_params = {
+        character: {
+        name: 'Jose',
+        age: '5',
+        enjoys: 'running',
+        image: 'http://d279m997dpfwgl.cloudfront.net/wp/2017/11/jose-running-1000x800.jpg'
+        }
+      }
+      post '/characters', params: character_params
+
+      expect(response).to have_http_status(200)
+
+      character = Character.first
+      expect(character.name). to eq 'Jose'
+    end
+  end
+
+  describe "PATCH /update" do
+    it"edits an existing character" do
+      Character.create name:'Jose', age: 5, enjoys: 'running', image: 'http://d279m997dpfwgl.cloudfront.net/wp/2017/11/jose-running-1000x800.jpg'
+
+      update_example = Character.first
+
+      character_params = {
+        character: {
+          name: 'Kendra',
+          age: 5,
+          enjoys: 'running',
+          image: 'http://d279m997dpfwgl.cloudfront.net/wp/2017/11/jose-running-1000x800.jpg'
+        }
+      }
+
+      patch "/characters/#{update_example.id}", params: character_params
+      character_mystery = Character.find(update_example.id)
+      expect(response).to have_http_status(200)
+      expect(character_mystery.name).to eq 'Kendra'
+    end
+  end
+
+
 end
